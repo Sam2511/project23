46d5fa792b248a621ae8490d3eade3fa833cfc37
Sam2511 <74780148+Sam2511@users.noreply.github.com>
2020-12-19 09:23:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,87 @@
+
+
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var line1,line2,line3;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(400, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(400, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+	
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+	 line1 = new line(400,660,80,10);
+	 line2 = new line(355,640,10,50);
+	 line3 = new line(445,640,10,50);
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+
+  
+
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+
+  
+
+  if(ground.y - packageSprite.y <= packageSprite.height/2 + ground.height/2){
+	packageSprite.velocityX = 0;
+  }
+
+
+  
+  drawSprites();
+  line1.display();
+  line2.display();
+  line3.display();
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
+	Body.setStatic(packageBody, isStatic=false);
+	helicopterSprite.velocityX = 5;
+  }
+}
+
+
+
